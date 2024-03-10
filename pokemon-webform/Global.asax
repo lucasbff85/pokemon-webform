4866fc8a37c9862a8c4a44f06f75b195d044dd1f
Lucas Boffa <[200~123970979+lucasbff85@users.noreply.github.com~>
2024-03-09 21:40:48 -0300

    comenzando con proyecto  webform vacio, dominio y negocio

---
+++ pokemon-webform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pokemon_webform.Global" Language="C#" %>
---
+++ pokemon-webform/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace pokemon_webform
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
